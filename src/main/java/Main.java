49cc1134e30e4dccdd19aa29b1031140f486758e
MetteMunch <meha0005@stud.kea.dk>
2024-03-04 10:07:16 +0100

    Opstart som owner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,13 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hej med jer");
         System.out.println("Hej hej");
+        System.out.println("Hej Luna");
 
 
-        System.out.println("Ny konflikt");
-        System.out.println("Ny konflikt sdfsggh");
 
-
-        System.out.println("Merge konflikt");
-
-        System.out.println("Vi tester total løst nu");
 
     }
 }
